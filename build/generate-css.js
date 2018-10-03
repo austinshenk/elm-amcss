7df9dd1d724c76f196bf2311ebdfe054cf1a13a4
Austin Shenk <austin.shenk@hannonhill.com>
2018-10-03 11:18:40 -0400

    Rewrite generate-css build file to not use async/await - Makes it compatible with older versions of node

--- build/generate-css.js
+++ build/generate-css.js
@@ -1,18 +1,22 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('file:///D:/documents/github/elm-amcss/build/css.html');
-
-  const css = await page.evaluate(() => {
+let _browser;
+let _page;
+let css;
+puppeteer.launch()
+.then(browser => _browser = browser)
+.then(browser => _page = browser.newPage())
+.then(page => page.goto('file:///Users/austinshenk/Documents/git/elm-amcss/build/css.html'))
+.then(() => _page)
+.then(page => {
+  return page.evaluate(() => {
     return document.body.firstChild.nextSibling.textContent;
-  });
-
-  await browser.close();
-
+  })
+})
+.then(content => css = content)
+.then(() => {
   fs.writeFile('build/index.css', css, (err) => {
     if (err) throw err;
   });
-})();
+})
+.then(() => _browser.close());
